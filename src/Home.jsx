cfa99421fa03aca92d13ccef3145bc0014529124
1saifu <saifukhan@hotmail.se>
2023-12-18 21:24:22 +0100

    New function

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
+import UserData from "./UserData";
 
 const Home = () => {
 
 const [users, setUsers] = useState([]);
+const [usersData, setUsersData] = useState(null)
 
 
-const getUserData = async () => {
+const fetchData = async () => {
     let resUser = await fetch('https://randomuser.me/api');
     let jsonUser = await resUser.json();
     console.log(jsonUser.results)
@@ -13,29 +15,39 @@ const getUserData = async () => {
 } 
 
 useEffect(() => {
-getUserData();
+fetchData();
 },[])
 
 const addFriend = () => {
-    getUserData();
+    fetchData();
 }
+
+const dataUsers = async (index) => {
+
+    const selectedUser = users[index]
+    setUsersData(selectedUser)
+
+}
+
 
 return(
     <div>
         <h3>Friends</h3>
         <ul className="container">
-        {users.map((user) => (
-            <li key={user.login.uuid}>
+        {users.map((user, index) => (
+            <li key={user.login.username}>
             <div>
             <img src={user.picture.large} alt="" width="150px" height="150px" />
             </div>
             <br />
             {user.name.title} {user.name.first} {user.name.last} 
             <br />
+            <button className="button" onClick={() => dataUsers(index)}>Visa info</button>
             <br />
             </li>
         ))}
         </ul>
+        {usersData.length > 0 && <UserData data={usersData} /> }
         <button className="button" onClick={addFriend}>Lägg till ny vän</button>
     </div>
 )
