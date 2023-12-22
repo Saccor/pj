d59ca688f1076f4a216fad445b43fdda97119ebc
1saifu <saifukhan@hotmail.se>
2023-12-22 15:55:07 +0100

    added feature

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ return(
         <h2>Home Page</h2>
 
         <h4>Recently added friends</h4>
-        <button className="button"><Link to="/Friends" style={{'textDecoration' : 'none'}}>Go to Friends</Link></button>
+        <Link to="/Friends" style={{'textDecoration' : 'none'}}>Go to Friends</Link>
     </div>
 )
 }
