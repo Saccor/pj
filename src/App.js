1eedb9951ea6b19bede4632410dd8d8ecfe22a6d
1saifu <saifukhan@hotmail.se>
2023-12-22 14:06:23 +0100

    Added adn changed new route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Friends from './Friends';
 import Home from './Home';
 import UserData from './UserData';
 import { Routes, Route } from "react-router-dom"
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/Friends' element={<Friends />}/>
       </Routes>
       </header>
     </div>
